3cc6e4d1c99e88317f8994ccf681a9d7f436460b
Xquantum398 <inpinitypinity@gmail.com>
2026-03-07 16:33:59 +0300

    Update Referer and Origin headers in app.py

--- app.py
+++ app.py
@@ -177,8 +177,8 @@ def proxy_m3u():
 
     default_headers = {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/33.0 Mobile/15E148 Safari/605.1.15",
-        "Referer": "https://vavoo.to/",
-        "Origin": "https://vavoo.to"
+        "Referer": "https://inattv1269.xyz/",
+        "Origin": "https://inattv1269.xyz/"
     }
 
     # Estrai gli header dalla richiesta, sovrascrivendo i default
